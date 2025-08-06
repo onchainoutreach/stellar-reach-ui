d44c7c9ffc27402a882f8b7e8ad1c46b5e5b0a7d
onchainoutreach <onchainoutreach@gmail.com>
2025-08-06 20:02:48 +0300

    Vercel direct links problem

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ }) => ({
+  build: {
+    outDir: 'dist'
+  },
   server: {
     host: "::",
     port: 8080,
